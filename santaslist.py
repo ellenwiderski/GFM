95e760b4de2a775eefbda38bca8b516a87d8fc91
ellenwiderski <wideel01@Ellens-MBP-3.dynamic.luther.edu>
2014-12-12 14:26:20 -0600

    more stuff

--- santaslist.py
+++ santaslist.py
@@ -34,8 +34,8 @@ class User(UserMixin):
         return '<User %s>' % (self.username)
 
     def add(self):
-        c = curs.execute('''INSERT INTO user values('%s','%s','%s',%s,%s);'''%(username,password,display_name,age,naughty))
-        g.conn.commit()
+        c = curs.execute('''INSERT INTO user values('%s','%s','%s',%s);'''%(self.username,self.password,self.display_name,self.naughty))
+        conn.commit()
 
 
 @login_manager.user_loader
@@ -48,13 +48,14 @@ def load_user(username):
 class LoginForm(Form):
 	username = TextField('Username',validators=[InputRequired()])
 	password = PasswordField('Password',validators=[InputRequired()])
+	remember = BooleanField('remember')
 
 class SignupForm(Form):
 	username = TextField('Username',validators=[InputRequired()])
 	password = PasswordField('Password',validators=[InputRequired()])
 	retypepassword = PasswordField('RetypePassword',validators=[InputRequired()])
 	display_name = TextField('DisplayName',validators=[InputRequired()])
-	naughtynice = RadioField('NaughtyOrNice',choices=[('naughty','Naughty'),('nice','Nice')])
+	naughtynice = RadioField('NaughtyOrNice',choices=[('naughty','Naughty'),('nice','Nice')],validators=[InputRequired()])
 
 class NewItem(Form):
 	newitem = TextField('additem')
@@ -66,6 +67,7 @@ def login():
 	if form.validate_on_submit():
 		g.user = form.username.data
 		g.password = form.password.data
+
 		c = curs.execute('''SELECT user_name FROM user WHERE user_name = '%s';''' % g.user)
 		userexists = c.fetchone()
 		if userexists:
@@ -73,7 +75,7 @@ def login():
 			correct = c.fetchone()
 			if correct:
 				user = load_user(g.user)
-				login_user(user)
+				login_user(user, remember=form.remember.data)
 				flash("logged in")
 				return redirect("/user/%s" % g.user)
 
@@ -84,36 +86,15 @@ def login():
 
 	return render_template('login.html',form=form)
 
-		#login_user(user)
-		#flash("logged in successfully")
-
-		# username = form.username.data
-		# password = form.password.data
-		# user = curs.execute('''SELECT user_name FROM user WHERE user_name = '%s' and password = '%s';'''% (username,password))
-		
-		# hasUser = False
-		# for i in user:
-		# 	hasUser = True
-
-		# if not hasUser:
-		# 	return render_template('login.html',form=form)
-		# else:
-		# 	resp = make_response(redirect('/user/%s' % username))
-		# 	resp.set_cookie('username',username)
-		# 	return resp
-
-		#return redirect('/user/%s' % username)
-
-	#return render_template('login.html',form=form)
-
 @app.route('/user/<username>', methods=['GET','POST'])
+@login_required
 def profile(username):
 
 	form = NewItem()
 	listdict = {}
 	lists = curs.execute('''SELECT list.list_id FROM user, list WHERE list.user_name = user.user_name and list.user_name='%s';'''%username)
 	for list in lists:
-		items = curs.execute('''SELECT item.item_name, item.item_price, item.item_link from item, list, list_item WHERE list_item.list_item_id = list.list_id and list.list_id = '%s';'''%list[0])
+		items = curs.execute('''SELECT item.item_name, item.item_price, item.item_link from item, list, list_item WHERE list_item.list_id = list.list_id and list.list_id = '%s';'''%list[0])
 		for item in items:
 			listdict[list[0]] = item[0]
 		
@@ -141,18 +122,18 @@ def signup():
 		password = form.password.data
 		display_name = form.display_name.data
 
-		if form.naughty.data:
-			naughty = True
+		if form.naughtynice.data == 'naughty':
+			naughty = 1
 		else:
-			naughty = False
+			naughty = 0
 
-		c = curs.execute('''SELECT user_name FROM user WHERE user_name = '%s';'''% username)
+		c = curs.execute('''SELECT user_name FROM user WHERE user_name = '%s';'''% user)
 		userexists = c.fetchone()
 
 		if not userexists:
 			newuser = User(user,password,display_name,naughty)
 			newuser.add()
-			return redirect('/user/%s' % username)
+			return redirect('/user/%s' % user)
 		else:
 			return render_template('signup.html',form=form)
 
---
+++ xmasapp.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+
+conn = sqlite3.connect("xmasapp.db") #create a connection (most databases are a client-server relationship, where requests are being sent and received
+
+curs = conn.cursor() #cursor is the position or what row we are on in the db - a pointer to where we are in the db
+
+curs.execute('''drop table user''')
+curs.execute('''Create table user (user_name text primary key, password text, display_name text, naughty boolean);''')
+
+curs.execute('''insert into user values ('MickeyMouse', 'passwordplease', 'Mickey', 1)''')
+
+
+curs.execute('''drop table item''')
+curs.execute('''Create table item (item_name text primary key, item_price int, item_link text);''')
+
+curs.execute('''insert into item values ('adventure pack', 50, 'www.patagonia.com')''')
+
+
+curs.execute('''drop table list''')
+curs.execute('''Create table list (list_id int primary key, list_name text, user_name text, foreign key(user_name) references user(user_name));''')
+
+curs.execute('''insert into list values (1,'Christmas List', 'MickeyMouse')''')
+
+
+curs.execute('''drop table list_item''')
+curs.execute('''Create table list_item (item_name text, list_id int, foreign key(item_name) references item(item_name), foreign key(list_id) references list(list_id));''')
+
+curs.execute('''insert into list_item values ('adventure pack', 1)''')
+
+conn.commit() #make this change permanent
