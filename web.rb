d264a2ce2387f08c6bd081875ae6e03915523dd5
ellenwiderski <wideel01@Ellens-MBP-3.dynamic.luther.edu>
2014-12-12 23:29:15 -0600

    test

--- web.rb
+++
@@ -1,9 +0,0 @@
-class App < Sinatra::Base
-  configure :production do
-    Sequel.connect ENV['HEROKU_POSTGRESQL_MAROON_URL']
-  end
-
-  configure :development do
-    Sequel.connect ENV['LOCAL_DATABASE_URL']
-  end
-end
