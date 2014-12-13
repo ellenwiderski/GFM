e9269f22f6f6c2f366c168b4fabb52e65860dc50
ellenwiderski <wideel01@Ellens-MBP-3.dynamic.luther.edu>
2014-12-12 23:08:42 -0600

    init

--- santaslist.py
+++ santaslist.py
@@ -1,7 +1,7 @@
 from flask import Flask, flash, request, make_response, render_template, redirect, flash, g
 from flask.ext.wtf import Form
-from flask.ext.login import LoginManager, login_user, UserMixin, logout_user
-from wtforms import TextField, PasswordField, IntegerField, BooleanField, RadioField
+from flask.ext.login import LoginManager, login_user, UserMixin, logout_user, login_required, current_user, AnonymousUserMixin
+from wtforms import TextField, PasswordField, IntegerField, BooleanField, RadioField, SelectField
 from wtforms.validators import InputRequired
 import sqlite3
 
@@ -37,6 +37,11 @@ class User(UserMixin):
         c = curs.execute('''INSERT INTO user values('%s','%s','%s',%s);'''%(self.username,self.password,self.display_name,self.naughty))
         conn.commit()
 
+class Anonymous(AnonymousUserMixin):
+  def __init__(self):
+    self.username = 'Guest'
+
+login_manager.anonymous_user = Anonymous
 
 @login_manager.user_loader
 def load_user(username):
@@ -58,8 +63,74 @@ class SignupForm(Form):
 	naughtynice = RadioField('NaughtyOrNice',choices=[('naughty','Naughty'),('nice','Nice')],validators=[InputRequired()])
 
 class NewItem(Form):
-	newitem = TextField('additem')
+	name = TextField('additem',validators=[InputRequired()])
+	forList = SelectField('forList')
 
+class NewList(Form):
+	name = TextField('newlist',validators=[InputRequired()])
+
+
+@app.route('/user/<username>', methods=['GET','POST'])
+@login_required
+def profile(username):
+	user = load_user(username)
+
+	newitem = NewItem()
+	newlist = NewList()
+
+	listdict = {}
+	choices = []
+
+	lists = curs.execute('''SELECT list.list_id,list.list_name FROM user, list WHERE list.user_name = user.user_name and list.user_name='%s';'''%username)
+	mylists = []
+
+	for mylist in lists:
+		mylists.append(mylist)
+
+	for mylist in mylists:
+		choices.append((mylist[1],mylist[1]))
+
+		items = curs.execute('''SELECT item.item_name, item.item_price, item.item_link from item, list, list_item WHERE list_item.list_id = list.list_id and list.list_id = '%s';'''%mylist[0])
+		
+		listitems = []
+
+		for item in items:
+			listitems.append(item)
+
+		listdict[mylist[1]] = listitems
+
+	newitem.forList.choices = choices
+
+	if newitem.validate_on_submit():
+		c = curs.execute('''SELECT list_id FROM list ORDER BY list_id DESC''')
+		highest = c.fetchone()[0]
+		curs.execute('''INSERT into item values('%s',50,'amazon.com')''' % newitem.name.data)
+		curs.execute('''INSERT into list_item values('%s',%s) '''%(newitem.name.data,highest+1))
+		conn.commit()
+		return redirect('/user/%s' % username)
+
+	if newlist.validate_on_submit() :
+		c = curs.execute('''SELECT list_id FROM list ORDER BY list_id DESC''')
+		highest = c.fetchone()[0]
+		curs.execute('''INSERT INTO list values(%s, '%s','%s')''' % (highest+1,newlist.name.data,username))
+		conn.commit()
+		return redirect('/user/%s' % username)
+
+	return render_template('profile.html',
+		listdict=listdict,
+		newitem=newitem,
+		newlist=newlist,
+		curruser=current_user.username,
+		profileuser=username,
+		curruserdisplay = current_user.display_name,
+		profileuserdisplay=user.display_name)
+
+
+@app.route('/', methods=['GET','POST'])
+def index():
+	if current_user.username != 'Guest':
+		return redirect('/user/%s' % current_user.username)
+	return redirect('/login')
 
 @app.route('/login', methods=['GET','POST'])
 def login():
@@ -76,7 +147,6 @@ def login():
 			if correct:
 				user = load_user(g.user)
 				login_user(user, remember=form.remember.data)
-				flash("logged in")
 				return redirect("/user/%s" % g.user)
 
 			else:
@@ -84,34 +154,12 @@ def login():
 		else:
 			return 'User does not exist'
 
-	return render_template('login.html',form=form)
+	return render_template('login.html',form=form,curruser=current_user)
 
-@app.route('/user/<username>', methods=['GET','POST'])
-@login_required
-def profile(username):
-
-	form = NewItem()
-	listdict = {}
-	lists = curs.execute('''SELECT list.list_id FROM user, list WHERE list.user_name = user.user_name and list.user_name='%s';'''%username)
-	for list in lists:
-		items = curs.execute('''SELECT item.item_name, item.item_price, item.item_link from item, list, list_item WHERE list_item.list_id = list.list_id and list.list_id = '%s';'''%list[0])
-		for item in items:
-			listdict[list[0]] = item[0]
-		
-		# if form.validate_on_submit():
-		# 	curs.execute('''INSERT into item values(form.newitem.data,50,"amazon.com")''')
-		# 	items = curs.execute('''SELECT list_item_id FROM list_item ORDER BY list_item_id DESC''')
-		# 	for i in items:
-		# 		highest = i[0]
-
-		# 	curs.execute('''INSERT into list_item values(%s,'%s',%s'''%(highest+1,form.newitem.data,listid))
-
-		# if i[0] == cookieusername:
-		# 	return render_template('profile.html',listdict=listdict,form=form,isUser=True)
-		# else:
-		# 	return render_template('profile.html',listdict=listdict,form=form,isUser=False)
-
-	return render_template('profile.html',listdict=listdict,form=form)
+@app.route('/logout', methods=['GET','POST'])
+def logout():
+	logout_user()
+	return redirect('/')
 
 @app.route('/signup',methods=['GET','POST'])
 def signup():
@@ -135,9 +183,9 @@ def signup():
 			newuser.add()
 			return redirect('/user/%s' % user)
 		else:
-			return render_template('signup.html',form=form)
+			return render_template('signup.html',form=form,curruser=current_user)
 
-	return render_template('signup.html',form=form)
+	return render_template('signup.html',form=form,curruser=current_user)
 
 if __name__ == '__main__':
    	app.run()
