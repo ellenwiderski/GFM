6bc104cc9b34a70af924c42bc8339786239b2f33
ellenwiderski <wideel01@Ellens-MBP-3.dynamic.luther.edu>
2014-12-12 23:21:53 -0600

    test

---
+++ app.rb
@@ -0,0 +1,16 @@
+configure :development do
+  set :database, "sqlite3:///database.db"
+end
+
+configure :production do
+  db = URI.parse(ENV['HEROKU_POSTGRESQL_WHITE_URL'] || 'postgres://localhost/mydb')
+
+  ActiveRecord::Base.establish_connection(
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
+end
